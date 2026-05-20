61212a4881284559be1173756cdc494b66e18a28
c3sk <francescvilaro@hotmail.com>
2026-05-20 17:29:43 +0200

    Add player ship class and rendering

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,6 +14,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,6 +26,7 @@ def main():
                 return
 
         screen.fill(pygame.Color("black"))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
