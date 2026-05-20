06e294accdcb2e1ffd00f20c9603f2334607db5b
c3sk <francescvilaro@hotmail.com>
2026-05-20 15:46:37 +0200

    Add game clock from frame rate control

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +21,9 @@ def main():
 
         screen.fill(pygame.Color("black"))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
