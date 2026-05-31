581ce5a17978fa3a3fb93d16f865795a37700eb7
c3sk <francescvilaro@hotmail.com>
2026-05-31 12:42:04 +0200

    Add player shooting capabilities

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid   
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
@@ -23,10 +24,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
 
     player = Player(x, y)
     asteroidfield = AsteroidField()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot 
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -15,6 +16,11 @@ class Player(CircleShape):
     rotated_vector = unit_vector.rotate(self.rotation)
     rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
     self.position += rotated_with_speed_vector
+
+  def shoot(self):
+    shot = Shot(self.position.x, self.position.y)
+    shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+    shot.velocity *= PLAYER_SHOOT_SPEED
 
   def triangle(self) -> list[pygame.Vector2]:
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -38,3 +44,5 @@ class Player(CircleShape):
         self.move(dt)
     if keys[pygame.K_s]:
         self.move(-dt)
+    if keys[pygame.K_SPACE]:
+        self.shoot()
